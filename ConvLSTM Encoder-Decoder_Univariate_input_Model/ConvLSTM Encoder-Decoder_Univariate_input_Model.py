74f1981d601e16cf2751824f7c5872e517c9bc20
Pranay Kumar <Pranay.kumar@infosys.com>
2019-04-23 03:55:48 +0530

    Add files via upload

---
+++ ConvLSTM Encoder-Decoder_Univariate_input_Model/ConvLSTM Encoder-Decoder_Univariate_input_Model.py
@@ -0,0 +1,250 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+# univariate multi-step encoder-decoder convlstm for the power usage dataset
+from math import sqrt
+from numpy import split
+from numpy import array
+from pandas import read_csv
+from sklearn.metrics import mean_squared_error
+from matplotlib import pyplot
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import Flatten
+from keras.layers import LSTM
+from keras.layers import RepeatVector
+from keras.layers import TimeDistributed
+from keras.layers import ConvLSTM2D
+
+from keras import Model, layers
+from keras.models import load_model, model_from_json
+
+
+# In[11]:
+
+
+# split a univariate dataset into train/test sets
+def split_dataset(data):
+    # split into standard weeks
+    train, test = data[1:-328], data[-328:-6]
+    # restructure into windows of weekly data
+    train = array(split(train, len(train)/7))
+    test = array(split(test, len(test)/7))
+    return train, test
+
+
+# In[12]:
+
+
+# evaluate one or more weekly forecasts against expected values
+def evaluate_forecasts(actual, predicted):
+    scores = list()
+    # calculate an RMSE score for each day
+    for i in range(actual.shape[1]):
+        # calculate mse
+        mse = mean_squared_error(actual[:, i], predicted[:, i])
+        # calculate rmse
+        rmse = sqrt(mse)
+        scores.append(rmse)
+    # calculate overall RMSE
+    s = 0
+    for row in range(actual.shape[0]):
+        for col in range(actual.shape[1]):
+            s += (actual[row, col] - predicted[row, col])**2
+    score = sqrt(s / (actual.shape[0] * actual.shape[1]))
+    return score, scores
+
+
+# In[13]:
+
+
+# summarize scores
+def summarize_scores(name, score, scores):
+    s_scores = ', '.join(['%.1f' % s for s in scores])
+    print('%s: [%.3f] %s' % (name, score, s_scores))
+
+
+# In[14]:
+
+
+# convert history into inputs and outputs
+def to_supervised(train, n_input, n_out=7):
+    # flatten data
+    data = train.reshape((train.shape[0]*train.shape[1], train.shape[2]))
+    X, y = list(), list()
+    in_start = 0
+    # step over the entire history one time step at a time
+    for _ in range(len(data)):
+        # define the end of the input sequence
+        in_end = in_start + n_input
+        out_end = in_end + n_out
+        # ensure we have enough data for this instance
+        if out_end < len(data):
+            x_input = data[in_start:in_end, 0]
+            x_input = x_input.reshape((len(x_input), 1))
+            X.append(x_input)
+            y.append(data[in_end:out_end, 0])
+        # move along one time step
+        in_start += 1
+    return array(X), array(y)
+
+
+# In[15]:
+
+
+# train the model
+def build_and_train_model(train, n_steps, n_length, n_input):
+    # prepare data
+    train_x, train_y = to_supervised(train, n_input)
+    # define parameters
+    verbose, epochs, batch_size = 1, 20, 16
+    n_features, n_outputs = train_x.shape[2], train_y.shape[1]
+    # reshape into subsequences [samples, timesteps, rows, cols, channels]
+    train_x = train_x.reshape((train_x.shape[0], n_steps, 1, n_length, n_features))
+    # reshape output into [samples, timesteps, features]
+    train_y = train_y.reshape((train_y.shape[0], train_y.shape[1], 1))
+    # define model
+    model = Sequential()
+    model.add(ConvLSTM2D(filters=64, kernel_size=(1,3), activation='relu', input_shape=(n_steps, 1, n_length, n_features)))
+    model.add(Flatten())
+    model.add(RepeatVector(n_outputs))
+    model.add(LSTM(200, activation='relu', return_sequences=True))
+    model.add(TimeDistributed(Dense(100, activation='relu')))
+    model.add(TimeDistributed(Dense(1)))
+    model.compile(loss='mse', optimizer='adam')
+    # fit network
+    model.fit(train_x, train_y, epochs=epochs, batch_size=batch_size, verbose=verbose)
+    return model
+
+
+# In[23]:
+
+
+# make a forecast
+def forecast(model, history, n_steps, n_length, n_input):
+    # flatten data
+    data = array(history)
+    data = data.reshape((data.shape[0]*data.shape[1], data.shape[2]))
+    # retrieve last observations for input data
+    input_x = data[-n_input:, 0]
+    # reshape into [samples, timesteps, rows, cols, channels]
+    input_x = input_x.reshape((1, n_steps, 1, n_length, 1))
+    # forecast the next week
+    yhat = model.predict(input_x, verbose=0)
+    # we only want the vector forecast
+    yhat = yhat[0]
+    return yhat
+
+
+# In[24]:
+
+
+# evaluate a single model
+def evaluate_model(train, test, n_steps, n_length, n_input):
+    # fit model
+    model = build_and_train_model(train, n_steps, n_length, n_input)
+    model.save('models/ConvLSTM_Encoder-Decoder_Univariate_input_Model.h5')
+
+    # architecture to JSON, weights to HDF5
+    model.save_weights('models/ConvLSTM_Encoder-Decoder_Univariate_input_Model_Weights.h5')
+    with open('models/ConvLSTM_Encoder-Decoder_Univariate_input_Model_architecture.json', 'w') as f:
+            f.write(model.to_json())
+    # history is a list of weekly data
+    history = [x for x in train]
+    # walk-forward validation over each week
+    predictions = list()
+    for i in range(len(test)):
+        # predict the week
+        yhat_sequence = forecast(model, history, n_steps, n_length, n_input)
+        # store the predictions
+        predictions.append(yhat_sequence)
+        # get real observation and add to history for predicting the next week
+        history.append(test[i, :])
+    # evaluate predictions days for each week
+    predictions = array(predictions)
+    score, scores = evaluate_forecasts(test[:, :, 0], predictions)
+    return score, scores
+
+
+# In[25]:
+
+
+def load_pretrained_model(train, test, n_steps, n_length, n_input):
+    # architecture and weights from HDF5
+    model = load_model('models/ConvLSTM_Encoder-Decoder_Univariate_input_Model.h5')
+
+    # architecture from JSON, weights from HDF5
+    with open('models/ConvLSTM_Encoder-Decoder_Univariate_input_Model_architecture.json') as f:
+        model = model_from_json(f.read())
+    model.load_weights('models/ConvLSTM_Encoder-Decoder_Univariate_input_Model_Weights.h5')
+    
+    
+    # history is a list of weekly data
+    history = [x for x in train]
+    # walk-forward validation over each week
+    predictions = list()
+    for i in range(len(test)):
+        # predict the week
+        yhat_sequence = forecast(model, history, n_steps, n_length, n_input)
+        # store the predictions
+        predictions.append(yhat_sequence)
+        # get real observation and add to history for predicting the next week
+        history.append(test[i, :])
+    # evaluate predictions days for each week
+    predictions = array(predictions)
+    score, scores = evaluate_forecasts(test[:, :, 0], predictions)
+    return score, scores
+
+
+# In[26]:
+
+
+# load the new file
+dataset = read_csv('smart meter dataset\household_power_consumption\household_power_consumption_Naive_Forecast_days.csv', header=0, infer_datetime_format=True, parse_dates=['datetime'], index_col=['datetime'])
+# split into train and test
+train, test = split_dataset(dataset.values)
+# define the number of subsequences and the length of subsequences
+n_steps, n_length = 2, 7
+# define the total days to use as input
+n_input = n_length * n_steps
+score, scores = evaluate_model(train, test, n_steps, n_length, n_input)
+# summarize scores
+summarize_scores('convlstm', score, scores)
+# plot scores
+days_label = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
+days = [0, 1, 2, 3, 4, 5, 6]
+pyplot.plot(days, scores, marker='o', label='convlstm')
+pyplot.xlabel(days_label)
+pyplot.ylabel("RMSE Daily Forecast Error")
+pyplot.show()
+
+
+# In[27]:
+
+
+# load pretrained model and get scores
+# define the number of subsequences and the length of subsequences
+n_steps, n_length = 2, 7
+# define the total days to use as input
+n_input = n_length * n_steps
+score, scores = load_pretrained_model(train, test, n_steps, n_length, n_input)
+# summarize scores
+summarize_scores('convlstm', score, scores)
+# plot scores
+days_label = ['sun', 'mon', 'tue', 'wed', 'thr', 'fri', 'sat']
+days = [0, 1, 2, 3, 4, 5, 6]
+pyplot.plot(days, scores, marker='o', label='convlstm')
+pyplot.xlabel(days_label)
+pyplot.ylabel("RMSE Daily Forecast Error")
+
+pyplot.show()
+
+
+# In[ ]:
+
+
+
+
